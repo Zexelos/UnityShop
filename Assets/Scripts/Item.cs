e51d4e2f87885d666a729c1b1ba08bf93bc6a46a
DESKTOP-HJMTSJF\Zexelos <zexelosg@gmail.com>
2020-12-30 15:28:38 +0100

    Some polish

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,7 +7,9 @@ public class Item : ScriptableObject
     [SerializeField] string description = default;
     [SerializeField] float weight = default;
     [SerializeField] float price = default;
+    [Header("Choose float between 0 and 1. 0 = 0%, 1 = 100%")]
     [SerializeField] float rollChance = 1f;
+
     public string Name => iName;
     public string Description => description;
     public float Weight => weight;
